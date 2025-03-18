e459643f71a0f00dde9a2acb980f8317c24f7fc2
markthepioneer <130753754+markthepioneer@users.noreply.github.com>
2025-03-18 18:54:26 -0400

    Update main application to include implemented modules

--- app/main.py
+++ app/main.py
@@ -7,11 +7,12 @@ from sqlalchemy.orm import Session
 from .database import engine, Base, get_db
 from . import models
 
-# Import all modules (will be uncommented as they are implemented)
-# from .finance_module import router as finance_router
-# from .health_module import router as health_router
-# from .task_module import router as task_router
-# from .portfolio_module import router as portfolio_router
+# Import all modules
+from .finance_module import router as finance_router
+from .health_module import router as health_router
+from .task_module import router as task_router
+from .portfolio_module import router as portfolio_router
+# These will be uncommented as they are implemented
 # from .memory_module import router as memory_router
 # from .email_module import router as email_router
 # from .life_balancer_module import router as life_balancer_router
@@ -35,11 +36,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Include all module routers (will be uncommented as they are implemented)
-# app.include_router(finance_router)
-# app.include_router(health_router)
-# app.include_router(task_router)
-# app.include_router(portfolio_router)
+# Include all module routers
+app.include_router(finance_router)
+app.include_router(health_router)
+app.include_router(task_router)
+app.include_router(portfolio_router)
+# These will be uncommented as they are implemented
 # app.include_router(memory_router)
 # app.include_router(email_router)
 # app.include_router(life_balancer_router)
