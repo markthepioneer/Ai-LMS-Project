3ec37503934af731e686cc09be578f88f767cb01
markthepioneer <130753754+markthepioneer@users.noreply.github.com>
2025-03-18 20:06:34 -0400

    Update main application to include all modules

--- app/main.py
+++ app/main.py
@@ -12,10 +12,9 @@ from .finance_module import router as finance_router
 from .health_module import router as health_router
 from .task_module import router as task_router
 from .portfolio_module import router as portfolio_router
-# These will be uncommented as they are implemented
-# from .memory_module import router as memory_router
-# from .email_module import router as email_router
-# from .life_balancer_module import router as life_balancer_router
+from .memory_module import router as memory_router
+from .email_module import router as email_router
+from .life_balancer_module import router as life_balancer_router
 
 # Create tables
 Base.metadata.create_all(bind=engine)
@@ -41,10 +40,9 @@ app.include_router(finance_router)
 app.include_router(health_router)
 app.include_router(task_router)
 app.include_router(portfolio_router)
-# These will be uncommented as they are implemented
-# app.include_router(memory_router)
-# app.include_router(email_router)
-# app.include_router(life_balancer_router)
+app.include_router(memory_router)
+app.include_router(email_router)
+app.include_router(life_balancer_router)
 
 # Root endpoint
 @app.get("/")
